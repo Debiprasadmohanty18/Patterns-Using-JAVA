4a5b6424b3bc3ca5692461fea0bcf5e2630c208c
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-22 21:19:36 +0530

    Add files via upload

---
+++ StarPattern2.java
@@ -0,0 +1,24 @@
+/* WAP to Print :-
+
+    * * * * *
+    * * * *
+    * * *
+    * * 
+    *
+           
+*/
+
+public class StarPattern2
+{
+    public static void main(String [] args)
+    {
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=i;j<5;j++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
